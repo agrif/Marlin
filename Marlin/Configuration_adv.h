960ee9deac8ce44cd21a1187b715179ed4979ccb
Aaron Griffith <aargri@gmail.com>
2017-11-11 13:12:54 -0500

    retract from 3mm to 7mm (same as old slic3r settings)

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -797,7 +797,7 @@
 #if ENABLED(FWRETRACT)
   #define MIN_AUTORETRACT 0.1             // When auto-retract is on, convert E moves of this length and over
   #define MAX_AUTORETRACT 10.0            // Upper limit for auto-retract conversion
-  #define RETRACT_LENGTH 3                // Default retract length (positive mm)
+  #define RETRACT_LENGTH 7                // Default retract length (positive mm)
   #define RETRACT_LENGTH_SWAP 13          // Default swap retract length (positive mm), for extruder change
   #define RETRACT_FEEDRATE 45             // Default feedrate for retracting (mm/s)
   #define RETRACT_ZLIFT 0                 // Default retract Z-lift
