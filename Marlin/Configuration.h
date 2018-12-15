04b58ecc95750474a231423ac218a4d3452bc1ae
Aaron Griffith <aargri@gmail.com>
2018-12-15 14:01:39 -0500

    backed back in to z probe offset, just a little bit

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -828,7 +828,7 @@
  */
 #define X_PROBE_OFFSET_FROM_EXTRUDER  -9   // X offset: -left  +right  [of the nozzle]
 #define Y_PROBE_OFFSET_FROM_EXTRUDER +22   // Y offset: -front +behind [the nozzle]
-#define Z_PROBE_OFFSET_FROM_EXTRUDER  -0.65 // Z offset: -below +above  [the nozzle]
+#define Z_PROBE_OFFSET_FROM_EXTRUDER  -0.70 // Z offset: -below +above  [the nozzle]
 
 // Certain types of probes need to stay away from edges
 #define MIN_PROBE_EDGE 10
