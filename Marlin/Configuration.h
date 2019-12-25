79eb7ddef9801038dc3e3d4ed2c4d649357e179b
Aaron Griffith <aargri@gmail.com>
2019-12-24 23:25:43 -0500

    new z probe mount, new z probe offset

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -947,7 +947,7 @@
  *
  * Specify a Probe position as { X, Y, Z }
  */
-#define NOZZLE_TO_PROBE_OFFSET { -9, +22, -2.40 }
+#define NOZZLE_TO_PROBE_OFFSET { -9, +22, -1.90 }
 
 // Certain types of probes need to stay away from edges
 #define MIN_PROBE_EDGE 10
